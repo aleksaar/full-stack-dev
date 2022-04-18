8803e6e4066c7e77a63772c3102547737480186e
aleksaar <aleksi.s.saarinen@gmail.com>
2022-04-18 22:08:10 +0300

    2.13 maat2

--- osa2/maat/src/OneCountry.js
+++ osa2/maat/src/OneCountry.js
@@ -1,20 +1,26 @@
-const OneCountry = ({country}) => {
-    return (
-        <>
-            <h1>{country.name.common}</h1>
-            <p>capital {country.capital[0]}</p>
-            <p>area {country.area}</p>
-            <h3>languages:</h3>
-            <ul>
-                {Object.values(country.languages).map((lang,i) => 
-                <li key={i}>
-                    {lang}
-                </li>
-                )}
-            </ul>
-            <img src={country.flags.png}/>
-        </>
-    )
+const OneCountry = ({country, show}) => {
+    if (show) {
+        return (
+            <>
+                <h1>{country.name.common}</h1>
+                <p>capital {country.capital[0]}</p>
+                <p>area {country.area}</p>
+                <h3>languages:</h3>
+                <ul>
+                    {Object.values(country.languages).map((lang,i) => 
+                    <li key={i}>
+                        {lang}
+                    </li>
+                    )}
+                </ul>
+                <img src={country.flags.png}/>
+            </>
+        )
+    }
+    else {
+        return (<></>)
+    }
+    
 }
 
 export default OneCountry
--- osa2/maat/src/components/ShowCountries.js
+++ osa2/maat/src/components/ShowCountries.js
@@ -1,26 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import OneCountry from "../OneCountry"
 
 
-
-
 const ShowCountries = ({countries}) => {
+    const [showBools, setShowBools] = useState([])
 
-    const clickHandler = (country) => {
-        console.log(country.name.common)
+    
+    const clickHandler = (i) => {
+        const newBools = [...showBools]
+        newBools[i] = !newBools[i]
+        setShowBools(newBools)
     }
 
+    const hook = () => {
+        setShowBools(new Array(countries.length).fill(false))
+        console.log("hook")
+    }
+    useEffect(hook, [countries])
 
     if (countries.length <= 10 && countries.length > 1) {
-
+    
         return (
             <>
             <ul>
-                {countries.map(country => 
-                <li key={country.name.common}>
+                {countries.map((country, i) => 
+                <div key={country.name.common}>
+                <li >
                     {country.name.common} 
-                    
+                    <button onClick={() => clickHandler(i)}>
+                        show
+                    </button>
                 </li>
+                <OneCountry country={country} show={showBools[i]} />
+                </div>
                 )}
             </ul>
             </>
@@ -30,7 +42,7 @@ const ShowCountries = ({countries}) => {
         console.log(countries[0])             
         return (
             <>
-                <OneCountry country={countries[0]} />
+                <OneCountry country={countries[0]} show={true}/>
             </>
         )
     }
